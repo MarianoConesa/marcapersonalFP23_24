f29c186de3776d2e96db7504dac00bf13d4c954a
Mariano <1784440@alu.murciaeduca.es>
2023-12-05 09:42:01 +0100

    Recogiendo datos de formularios

--- app/Http/Controllers/EstudianteController.php
+++ app/Http/Controllers/EstudianteController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Estudiante;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class EstudianteController extends Controller
 {
@@ -23,12 +25,26 @@ class EstudianteController extends Controller
             ->with("estudiante", Estudiante::findOrFail($id));
     }
 
-    public function putEdit($id) {
-        return view('estudiantes.edit')
-            ->with("estudiante", Estudiante::findOrFail($id));
+    public function putEdit(Request $request, $id) {
+
+        $estudiante = Estudiante::findOrFail($id);
+        $estudiante->update($request->all());
+
+        return redirect(action([self::class, 'getShow'], ['id' => $id]));
     }
 
     public function getCreate(){
         return view('estudiantes.create');
+    }
+
+    public function store(Request $request){
+        //dd($request->all());
+        //$name = $request->input('name');
+
+        $estudiante = Estudiante::create($request->all());
+
+        return redirect(action([self::class, 'getShow'], ['id' => $estudiante->id]));
+
+
     }
 }
